f835496f0244f282d95e31935ebdf1e5a3f3662a
David Cantoran <davidcantoran36@gmail.com>
2020-03-12 22:07:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,15 +1,32 @@
+# require_relative 'config/environment'
+
+# class App < Sinatra::Base
+#     get '/name' do 
+#       "My name is __"
+#     end 
+
+#     get '/hometown' do 
+#       "My hometown is __"
+#     end 
+
+#     get '/favorite-song' do 
+#       "My favorite song is __"
+#     end
+# end
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-    get '/name' do 
-      "My name is __"
-    end 
 
-    get '/hometown' do 
-      "My hometown is __"
-    end 
+  get '/name' do 
+    "My name is Victoria"
+  end
 
-    get '/favorite-song' do 
-      "My favorite song is __"
-    end
+  get '/hometown' do 
+    "My hometown is Charlottesville"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Shake It Off"
+  end
 end
